92100ca0ab6716814cc4278bb90237869568ee9e
ammargif <161625541+ammargif@users.noreply.github.com>
2024-10-30 13:13:28 +0700

    Update FoodOrder.java

--- ETS/FoodOrder/FoodOrder.java
+++ ETS/FoodOrder/FoodOrder.java
@@ -1,28 +1,25 @@
 import java.util.ArrayList;
 
 public class FoodOrder {
-    private ArrayList<String> menuItems;
-    private ArrayList<Double> itemPrices;
+    private ArrayList<MenuItem> menuItems;
     private double totalPrice;
     private boolean isPaid;
 
     public FoodOrder() {
         menuItems = new ArrayList<>();
-        itemPrices = new ArrayList<>();
         totalPrice = 0.0;
         isPaid = false;
     }
 
-    public void addMenuItem(String item, double price) {
+    public void addMenuItem(MenuItem item) {
         menuItems.add(item);
-        itemPrices.add(price);
         calculateTotal();
     }
 
     public void calculateTotal() {
         totalPrice = 0.0;
-        for (double price : itemPrices) {
-            totalPrice += price;
+        for (MenuItem item : menuItems) {
+            totalPrice += item.getPrice();
         }
     }
 
@@ -34,7 +31,7 @@ public class FoodOrder {
         return totalPrice;
     }
 
-    public ArrayList<String> getMenuItems() {
+    public ArrayList<MenuItem> getMenuItems() {
         return menuItems;
     }
 
@@ -44,8 +41,8 @@ public class FoodOrder {
 
     public void showOrder() {
         System.out.println("Pesanan:");
-        for (int i = 0; i < menuItems.size(); i++) {
-            System.out.println("- " + menuItems.get(i) + ": Rp" + itemPrices.get(i));
+        for (MenuItem item : menuItems) {
+            System.out.println("- " + item.getName() + ": Rp" + item.getPrice());
         }
         System.out.println("Total Harga: Rp" + totalPrice);
         System.out.println("Status Pembayaran: " + (isPaid ? "Sudah Dibayar" : "Belum Dibayar"));
